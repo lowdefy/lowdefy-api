707a0f7e8730c7e00932ded139606697cf4764c7
SamTolmay <samtolmay@gmail.com>
2020-12-10 11:35:50 +0200

    chore: test serverless ci

--- src/routes/hello/get/resolver.js
+++ src/routes/hello/get/resolver.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 async function hello({ body, context }) {
-  return { code: 200, status: 'Success', message: 'Hello' };
+  return { code: 200, status: 'Success', message: 'Hello from serverless' };
 }
 
 export default hello;
