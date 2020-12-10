c4ee68b625714d8e72e49408597a8066f1e4e436
SamTolmay <samtolmay@gmail.com>
2020-12-10 12:06:51 +0200

    chore: add ci tests

---
+++ README.md
@@ -0,0 +1,6 @@
+### Lowdefy Telemetry Api
+
+![Tests Main](https://github.com/lowdefy/telemetry-api/workflows/Test%20Branches/badge.svg?branch=main)
+![Tests Develop](https://github.com/lowdefy/telemetry-api/workflows/Test%20Branches/badge.svg?branch=develop)
+[![Maintainability](https://api.codeclimate.com/v1/badges/69945f3c3fee5b095f5a/maintainability)](https://codeclimate.com/github/lowdefy/telemetry-api/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/69945f3c3fee5b095f5a/test_coverage)](https://codeclimate.com/github/lowdefy/telemetry-api/test_coverage)
--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,9 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js'],
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/test/', '<rootDir>/src/shell/'],
-  coverageReporters: [['lcov', { projectRoot: '../../..' }], 'text', 'clover'],
+  coveragePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/test/'],
+  coverageReporters: ['lcov', 'text', 'clover'],
   errorOnDeprecated: true,
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/.lowdefy/'],
+  testPathIgnorePatterns: ['<rootDir>/dist/'],
 };
